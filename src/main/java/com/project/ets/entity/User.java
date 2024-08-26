21456344df6d6a2d01a98f1b20c12d776a6bd3b7
unknown <saiswaroop17254@gmail.com>
2024-08-26 11:27:26 +0530

    user entity created

---
+++ src/main/java/com/project/ets/entity/User.java
@@ -0,0 +1,41 @@
+package com.project.ets.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+	@Id
+	@Column(name = "userId")
+	private int userId;
+	
+	@Column(name = "user_name")
+	private String username;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "role")
+	private String role;
+	
+	@Column(name = "createdDate")
+	private LocalDateTime createdDate;
+	
+	@Column(name = "modifiedDate")
+	private LocalDateTime modifiedDate;
+
+}
